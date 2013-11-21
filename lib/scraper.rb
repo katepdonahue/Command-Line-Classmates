c5d077f7c11b855c48cbbab7555cb70c4231acc1
Kate <katepdonahue@gmail.com>
2013-11-21 16:10:49 -0500

    Make methods in the scraper to find the twitter handle and the blog url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,33 @@
-require 'open-uri'
-require 'nokugiri'
+# require the libraries we need
+require 'open-uri' # open the file on the internet
+require 'nokogiri' # line10 
 
 class Scraper
-  attr_reader :download
+  attr_reader :html
 
   def initialize url
     download = open(url)
+    @html = Nokogiri::HTML(download)
+  end
 
+  def get_students_names
+    html.search("h3").text
+  end
+
+  def get_twitter
+    mixed_array = html.search("li:first-child a").text.split(" ")
+    twitter_array = mixed_array.select { |name| name[0] == "@"}
+  end
+
+  def get_blog
+    blog_url = []
+    19.times do |i|
+      blog_url << html.search("a.blog[href]")[i]["href"]
+    end
+    blog_url
   end
 
 end
+
+my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+puts my_scraper.get_blog
